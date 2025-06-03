5fea4bc53d3d0dd154b8a0059d16033cc8571ce3
Yukon <ukowork@gmail.com>
2025-06-03 15:34:59 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/hex-final-2025/'
-    : '/',
+  publicPath: '/hex-final-2025/',
   chainWebpack: config => {
     config.module
       .rule('md')
