745ed946453dd60dc9ab7ab172a42764c4fa06f5
Yukon <ukowork@gmail.com>
2025-06-03 18:28:21 +0800

    blog active

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -2,7 +2,7 @@
   <header class="sticky top-0 z-50 mx-auto my-0 w-full bg-wrap py-6">
     <nav class="flex justify-center gap-x-10">
       <router-link :class="['nav-link', $route.path === '/' && 'text-brand']" to="/">首頁</router-link>
-      <router-link :class="['nav-link', $route.path === '/blog' && 'text-brand']" to="/blog">部落格</router-link>
+      <router-link :class="['nav-link', $route.path.includes('blog') && 'text-brand']" to="/blog">部落格</router-link>
     </nav>
   </header>
 </template>
